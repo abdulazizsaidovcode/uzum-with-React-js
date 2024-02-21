8f5323a075f5413adffb8327a4a53280e565cf62
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-02-21 19:54:38 +0500

    lsaom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+import styled from "styled-components";
 
 function App() {
   return (
     <>
     <Container>
-      
+      <div>nimadi</div>
     </Container>
     </>
   );
 }
 
-const Container = styled.div `
-
+const Container = styled.i `
+  color: red;
+  text-align: center;
 `
 export default App;
 
