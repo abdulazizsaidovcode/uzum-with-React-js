c1df60c1691d5d3604d433dfb43e85a81a565a30
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-02-22 14:44:41 +0500

    salom

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,12 +5,26 @@ function Navbar() {
     return (
         <nav className="w-full h-12 py-3">
             <div className="container flex items-center">
-                    <img className="h-10" src={logo} alt="" />
-                <div className="flex items-center">
-                    <NavbarButton/>
+                <img className="h-10" src={logo} alt="" />
+                <div className="flex items-center ml-6">
+                    <NavbarButton />
                 </div>
-                <div className="flex items-center">
-                    <NavbarInput/>
+                <div className="flex items-center ml-5 w-[40%]">
+                    <NavbarInput />
+                </div>
+                <div className="ml-6 flex items-center justify-between w-[40%]">
+                    <div className="flex items-center">
+                        <i class="ri-user-line text-xl mr-2"></i>
+                        <p>User name</p>
+                    </div>
+                    <div className="flex">
+                        <i class="ri-heart-line text-xl mr-2"></i>
+                        <p>User name</p>
+                    </div>
+                    <div className="flex">
+                        <i class="ri-shopping-basket-line text-xl mr-2"></i>
+                        <p>User name</p>
+                    </div>
                 </div>
             </div>
         </nav>
