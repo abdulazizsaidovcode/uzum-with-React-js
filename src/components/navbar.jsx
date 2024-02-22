f14ca754c3c7e82b09750eb964785f6f07fffa7a
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-02-22 13:02:05 +0500

    salom

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,13 @@
 import logo from "../assets/Uzum logo.png";
+import NavbarButton from "./tools/btns/Navbar button";
 function Navbar() {
     return (
         <nav className="w-full h-12 py-3">
-            <div className="container">
-                <div>
-                    <img className="h-12" src={logo} alt="" />
-                </div>
-                <div>
-                    
+            <div className="container flex items-center">
+                    <img className="h-10" src={logo} alt="" />
+                <div className="flex items-center">
+                    <NavbarButton/>
+                    <h1>slaom</h1>
                 </div>
             </div>
         </nav>
--- src/components/tools/btns/Navbar button/index.jsx
+++ src/components/tools/btns/Navbar button/index.jsx
@@ -1,8 +1,16 @@
-
-function NavbarButton(){
-    return(
-        <button>
-            <i class="fa-solid fa-rectangle-history"></i>
+import buttonimg from "../../../../assets/btnimg.png"
+function NavbarButton() {
+    const button = {
+        border: "2px solid #7101ff ",
+        borderRadius: "10px",
+        width: "160px",
+        height: "50px",
+        color: "#7101ff"
+    }
+    return (
+        <button style={button} className={`flex item-end`}>
+            <img className="h-full" src={buttonimg} alt="logobutton" />
+            <p>Каталог</p>
         </button>
     )
 }
