f36c2e5e248e9b1b0329840cf01effeb00d09c25
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-02-24 17:18:16 +0500

    slaom

--- src/components/pages/Cart.jsx
+++ src/components/pages/Cart.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import product from "../json/product"
 import DeliveryButton from "../tools/btns/delivery"
+import ProductCard from "../tools/cards"
 
 function Cart() {
     const [products, getGroduct] = useState(null)
@@ -43,7 +44,7 @@ function Cart() {
                                     <DeliveryButton text={"25 февраля (Завтра)"} />
                                 </div>
                             </div>
-                        ) }
+                        )}
                         {basket && basket.map((item, index) => (
                             <div className="mb-2" key={index}>
                                 <div className="border w-full my-5"></div>
@@ -129,24 +130,7 @@ function Cart() {
                 <h1 className="text-2xl mb-5">С этими товарами покупают</h1>
                 <div className="w-full grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-5 ">
                     {products && products.map((item, index) => (
-                        <div key={index} className="pb-10">
-                            <div className="w-full h-full hover:shadow-md  overflow-hidden rounded-md">
-                                <div className='w-full h-max relative overflow-hidden'>
-                                    <img className='w-full h-84 bg-contain hover:scale-105 transition-all' src={item.img} alt="Image 1" />
-                                    <button className="absolute bottom-0 left-0 bg-violet-700 text-white rounded-md px-3">Акция</button>
-                                </div>
-                                <div className="product-description p-3">
-                                    <p className="text-sm">{item.describtion}</p>
-                                    <div className="flex ">
-                                        <p><i class="ri-star-fill text-yellow-400 mr-1"></i>`{item.comments}`</p>
-                                        <p>{item.rate}</p>
-                                    </div>
-                                    <p>{item.monthpay}</p>
-                                    <p>{item.price}</p>
-                                    <p>{item.oldprice}</p>
-                                </div>
-                            </div>
-                        </div>
+                        <ProductCard img={item.img} describtion={item.describtion} comments={item.comments} rate={item.rate} monthpay={item.monthpay} price={item.price} oldprice={item.oldprice} />
                     ))}
                 </div>
 
--- src/components/tools/cards/index.jsx
+++ src/components/tools/cards/index.jsx
@@ -1,35 +1,29 @@
-import { useEffect, useState } from "react";
-import product from "../json/product";
 
 
 
 
-function ProductCard(count, img, describtion, comments, rate, monthpay, price, oldprice) {
+
+function ProductCard({img, describtion, comments, rate, monthpay, price, oldprice}) {
 
     return (
-        <section>
-            <div className='product w-full grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-5 '>
-                <div className="pb-10">
-                    <div className="w-full h-full hover:shadow-md  overflow-hidden rounded-md">
-                        <div className='w-full h-max relative overflow-hidden'>
-                            <img className='w-full h-84 bg-contain hover:scale-105 transition-all' src={img} alt="Image 1" />
-                            <button className="absolute bottom-0 left-0 bg-violet-700 text-white rounded-md px-3">Акция</button>
-                        </div>
-                        <div className="product-description p-3">
-                            <p className="text-sm">{describtion}</p>
-                            <div className="flex ">
-                                <p><i class="ri-star-fill text-yellow-400 mr-1"></i>`{comments}`</p>
-                                <p>{rate}</p>
-                            </div>
-                            <p>{monthpay}</p>
-                            <p>{price}</p>
-                            <p>{oldprice}</p>
-                        </div>
-                    </div>
+        <div className="pb-10">
+            <div className="w-full h-full hover:shadow-md  overflow-hidden rounded-md">
+                <div className='w-full h-max relative overflow-hidden'>
+                    <img className='w-full h-84 bg-contain hover:scale-105 transition-all' src={img} alt="Image 1" />
+                    <button className="absolute bottom-0 left-0 bg-violet-700 text-white rounded-md px-3">Акция</button>
                 </div>
-            </div >
-        </section>
-
+                <div className="product-description p-3">
+                    <p className="text-sm">{describtion}</p>
+                    <div className="flex ">
+                        <p><i class="ri-star-fill text-yellow-400 mr-1"></i>`{comments}`</p>
+                        <p>{rate}</p>
+                    </div>
+                    <p>{monthpay}</p>
+                    <p>{price}</p>
+                    <p>{oldprice}</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
