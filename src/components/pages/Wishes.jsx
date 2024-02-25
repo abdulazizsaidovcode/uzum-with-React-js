ac9a2478667b7e23336bbc39abaff11b12702395
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-02-25 11:57:23 +0500

    slaom

--- src/components/pages/Wishes.jsx
+++ src/components/pages/Wishes.jsx
@@ -1,16 +1,44 @@
 import ProductCard from "../tools/cards"
-import products from "../json/product"
+import product from "../json/product"
 import Empty from "../tools/empty"
 import EmptyImg from "../../assets/empty/not found.png"
+import { useEffect, useState } from "react"
+
+
 function Wishes() {
+
+    const [products, getProduct] = useState()
+
+    useEffect(() => {
+        getProducts()
+    }, [])
+
+    function getProducts() {
+        getProduct(product.splice(0, 1))
+    }
+
     return (
         <section className="container">
-            <div className={`${!products ? "grid" : ""} w-full  lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-5 `}>
-                {!products ? products.map((item) => (
+
+            {/* products */}
+            {products &&
+                <div className="mb-5">
+                    <p>Мои желания</p>
+                    <div className="w-full border"></div>
+                </div>}
+            <div className={`${products ? "grid" : ""} w-full  lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-5 `}>
+                {products ? products.map((item, index) => (
                     <ProductCard img={item.img} describtion={item.describtion} comments={item.comments} rate={item.rate} monthpay={item.monthpay} price={item.price} oldprice={item.oldprice} />
-                )) : <Empty img={EmptyImg} tite={'Добавьте то, что понравилось'} description={'Нажмите на ♡ в товаре. Войдите в аккаунт и всё избранное сохранится'} btnText={'Войти в аккаунт'}/>}
+                )) : <Empty img={EmptyImg} tite={'Добавьте то, что понравилось'} description={'Нажмите на ♡ в товаре. Войдите в аккаунт и всё избранное сохранится'} btnText={'Войти в аккаунт'} />}
+            </div>
+
+            {/* populary products */}
+            <div className={`grid w-full  lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-5 `}>
+                {products && products.map((item, index) => (
+                    <ProductCard img={item.img} describtion={item.describtion} comments={item.comments} rate={item.rate} monthpay={item.monthpay} price={item.price} oldprice={item.oldprice} />
+                ))}
             </div>
         </section>
     )
 }
-export default Wishes
+export default Wishes;
--- src/components/tools/empty/index.jsx
+++ src/components/tools/empty/index.jsx
@@ -1,11 +1,12 @@
 import DefaultBtn from "../btns/default";
 
-function Empty({img, tite, description, btnText}){
-    return(
-        <div className="w-full h-96 flex justify-center items-center flex-col">
-            <img src={img} alt="rasm" className="w-40"/>
-            <p>{tite}</p>
-            <DefaultBtn name={btnText}/>
+function Empty({ img, tite, description, btnText }) {
+    return (
+        <div className="w-full h-[500px] flex justify-center items-center flex-col">
+            <img src={img} alt="rasm" className="w-36" />
+            <p className="mb-5">{tite}</p>
+            <p className="mb-5">{description}</p>
+            <DefaultBtn name={btnText} />
         </div>
     )
 }
