03d8d91d2c2a25dd24f21f22f738f1192fab3bb3
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-02-26 12:38:24 +0500

    salom

--- src/components/tools/cards/index.jsx
+++ src/components/tools/cards/index.jsx
@@ -1,16 +1,18 @@
 
-
+import uzumbasket from "../../../assets/icons/uzum-basket.png"
+import heart from "../../../assets/icons/heart.png"
 
 
 
 function ProductCard({ img, describtion, comments, rate, monthpay, price, oldprice }) {
 
     return (
-        <div className="pb-10">
-            <div className="w-full h-full hover:shadow-md  overflow-hidden rounded-md">
-                <div className='w-full h-max relative overflow-hidden'>
-                    <img className='w-full h-84 bg-contain hover:scale-105 transition-all' src={img} alt="Image 1" />
+        <div className="mb-10 cursor-pointer">
+            <div className="w-full h-full hover:shadow-md overflow-hidden rounded-md">
+                <div className='w-full h-80 relative overflow-hidden'>
+                    <img className='w-full h-full bg-contain hover:scale-105 transition-all' src={img} alt="Image 1" />
                     <button className="absolute bottom-0 left-0 bg-violet-700 text-white rounded-md px-3">Акция</button>
+                    <button className=" absolute w-8 h-8 top-2 right-2 text-xl"><img src={heart} class="ri-heart-line"/></button>
                 </div>
                 <div className="product-description p-3">
                     <p className="text-sm">{describtion}</p>
@@ -21,12 +23,12 @@ function ProductCard({ img, describtion, comments, rate, monthpay, price, oldpri
                     <div className="bg-yellow-200 w-max px-2 text-xs mt-1 rounded-md">
                         <p>{monthpay} сум/мес</p>
                     </div>
-                    <div>
+                    <div className="w-full flex justify-between items-center mt-6">
                         <div>
-                            <p>{price}</p>
-                            <p>{oldprice}</p>
+                            <del className="text-gray-400 text-xs">{price} сум</del>
+                            <p>{oldprice} сум</p>
                         </div>
-                        <button></button>
+                        <button className="w-10 h-10 rounded-full border "><img src={uzumbasket} alt="" className="w-full hover:bg-gray-100 p-1 transition-all rounded-full" /></button>
                     </div>
                 </div>
             </div>
