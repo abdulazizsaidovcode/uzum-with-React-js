54a52e0c0b04bd383be29a9fb8123e026e0e1f2e
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-02-27 20:14:53 +0500

    adwansed slide end slide

--- src/components/pages/Product.jsx
+++ src/components/pages/Product.jsx
@@ -1,24 +1,91 @@
+import React, { useEffect, useRef } from 'react';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/splide.min.css';
 
-function Product() {
+// Rasmlarni import qilish kerak:
+import mainImage from "../../assets/img/Product/croduct1.jpeg"; // Asosiy rasm
+import thumbnailImage from "../../assets/img/Product/product 1.jpeg"; // Kichik rasm
 
-    return (
-        <main className='container'>
-            {/* product page short description */}
-            <div className='grid grid-cols-5'>
-                {/* slider  */}
-                <div className=' col-span-3 w-full'></div>
-                {/*  description  */}
-                <div className=' col-span-2'>
+const Product = () => {
+  const mainSliderRef = useRef();
+  const thumbnailsRef = useRef();
 
-                </div>
-            </div>
+  useEffect(() => {
+    if (mainSliderRef.current && thumbnailsRef.current) {
+      const mainSplide = mainSliderRef.current.splide;
+      const thumbsSplide = thumbnailsRef.current.splide;
 
-            {/* product full description */}
+      mainSplide.sync(thumbsSplide);
+    }
+  }, [mainSliderRef, thumbnailsRef]);
 
-        </main>
-    )
-}
+  return (
+    <div className="splide-wrapper flex flex-row">
+      <div className="thumbnails-splide felx-1">
+        <Splide
+          ref={thumbnailsRef}
+          options={{
+            direction: 'ttb', // Vertikal yo'nalish
+            height: '600px', // Vertikal slayder uchun balandlik sozlanishi
+            fixedWidth: 104,
+            fixedHeight: 58,
+            isNavigation: true,
+            gap: 10,
+            focus: 'center',
+            pagination: false,
+            cover: true,
+            dragMinThreshold: {
+              mouse: 4,
+              touch: 10,
+            },
+            breakpoints: {
+              640: {
+                fixedWidth: 66,
+                fixedHeight: 38,
+              },
+            },
+          }}
+        >
+          {/* Kichik rasmlar */}
+          <SplideSlide>
+            <img src={thumbnailImage} alt="Kichik rasm 1" />
+          </SplideSlide>
+          <SplideSlide>
+            <img src={thumbnailImage} alt="Kichik rasm 1" />
+          </SplideSlide>
+          <SplideSlide>
+            <img src={thumbnailImage} alt="Kichik rasm 1" />
+          </SplideSlide>
+          {/* Boshqa kichik rasmlar qo'shilishi mumkin */}
+        </Splide>
+      </div>
 
-
+      <div className="main-splide flex-3">
+        <Splide
+          ref={mainSliderRef}
+          options={{
+            type: 'fade',
+            heightRatio: 0.5,
+            pagination: false,
+            arrows: false,
+            cover: true,
+          }}
+        >
+          {/* Asosiy rasmlar */}
+          <SplideSlide>
+            <img src={mainImage} alt="Asosiy rasm 1" />
+          </SplideSlide>
+          <SplideSlide>
+            <img src={mainImage} alt="Asosiy rasm 1" />
+          </SplideSlide>
+          <SplideSlide>
+            <img src={mainImage} alt="Asosiy rasm 1" />
+          </SplideSlide>
+          {/* Boshqa asosiy rasmlar qo'shilishi mumkin */}
+        </Splide>
+      </div>
+    </div>
+  );
+};
 
 export default Product;
